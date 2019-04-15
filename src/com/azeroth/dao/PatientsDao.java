084fb056ab02b26acf4b9889a358aafb9626e244
QianJie <49468844+bbod-ykk@users.noreply.github.com>
2019-04-15 16:27:56 +0900

    Add files via upload

--- src/com/azeroth/dao/PatientsDao.java
+++ src/com/azeroth/dao/PatientsDao.java
@@ -11,5 +11,6 @@ public interface PatientsDao {
 	void  modPatient(Patients pa);
 	List<Patients> findAll();
 	List<Patients> findByName(String pi_name);
+	List<Patients> findById(String pi_id);
 	
 }
